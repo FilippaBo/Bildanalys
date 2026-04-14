2b11ca7732ce18b700e3af49f95423b715fcd6f9
corneliaforss <corneliaforss@Corres-MacBook-Air.local>
2026-04-14 11:24:18 +0200

    Oscars ändringar

--- ovning1april.m
+++ ovning1april.m
@@ -1,15 +1,24 @@
-load lungCT_slice.mat
-whos
-max(max(lungCT_slice))-min(min(lungCT_slice))
-imshow(lungCT_slice,[])
-imtool(lungCT_slice)
+img_raw = 'Xray.png';
 
+%% Gör om bild till 0-1 värden
+img_pixels = imread(img_raw);
+img_0to1 = im2double(img_pixels);
 
-%%
-imadjdemo
+[M, N] = size(img_0to1);
 
+%% 
+PSF_length = 60;
+PSF_angle = 0;
+PSF_true = fspecial("motion",PSF_length,PSF_angle);
 
-%%
+img_distort = imfilter(img_0to1, PSF_true, "circular", "conv");
 
-hej 
+subplot(1,2,1); 
+imshow(img_0to1);
 
+subplot(1,2,2); 
+imshow(img_distort);
+
+%% Add noise (Skipped for now)
+
+%% Psuedoinverse filter
