9530777817d9003125020bead3dc766ceeca2298
corneliaforss <corneliaforss@Corres-MacBook-Air.local>
2026-04-21 14:25:37 +0200

    lagt alla bilder brevid varandra i en tabell så att man kan se både bilddomän och fourierdomän

--- project4_g4.m
+++ project4_g4.m
@@ -25,26 +25,38 @@ PSF_length = 60;
 PSF_angle = 0;
 PSF_true = fspecial("motion",PSF_length,PSF_angle);
 
-img_distort_ft = fft2(img_0to1);
-PSF_fourier = psf2otf(PSF_true, [M,N]);
-img_blurred_ft = img_distort_ft .* PSF_fourier ;
-img_blurred = ifft2(img_blurred_ft);
+img_distort_ft = fft2(img_0to1); %gör om bilden till fourierdomänen så f-> F
+PSF_fourier = psf2otf(PSF_true, [M,N]); %gör om psf till fouriervärden h -> H
+img_blurred_ft = img_distort_ft .* PSF_fourier ; %F * H = G
+img_blurred = ifft2(img_blurred_ft); % G -> g
 
 
-subplot(1,2,1); 
-imshow(img_0to1);
+subplot(2,3,1); 
+imshow(img_0to1); %originalbild f
 
-subplot(1,2,2); 
-imshow(img_blurred);
+subplot(2,3,4);
+imshow(log(abs(fftshift(img_distort_ft))),[]); %originalbild i ft, F
+
+subplot(2,3,2);
+imshow(PSF_true); % h 
+
+subplot(2,3,5);
+imshow(log(abs(fftshift(PSF_fourier))),[]); % H
+
+subplot(2,3,3); 
+imshow(img_blurred); % blurrad bild, g
+ 
+subplot(2,3,6);
+imshow(log(abs(fftshift(img_blurred_ft))),[]); %blurrad bild i ft, G
+
 
 %% Add noise (Skipped for now)
+
 
 %% Psuedoinverse filter
 %ändrat till att räkna med fouriertransformerade blurrade bilden
 
-PSF_fourier = psf2otf(PSF_true, [M,N]) %gör om psf till fouriervärden
 img_restored = ifft2(img_blurred_ft ./PSF_fourier);
 imshow(img_restored);
 
 %%
-%pesudoinverse 
