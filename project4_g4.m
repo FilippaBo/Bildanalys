a6c84bafcfba744682150832e312de1a8808bb92
corneliaforss <corneliaforss@Corres-MacBook-Air.local>
2026-04-21 13:54:40 +0200

    Har ändrat att vi nu beräknar en distortade bilden med fouriertransform istället för imfilter funktionen

--- project4_g4.m
+++ project4_g4.m
@@ -19,10 +19,32 @@ imshow(img_0to1);
 subplot(1,2,2); 
 imshow(img_distort);
 
+%%
+% nytt sätt att blurra fast med fourier 
+PSF_length = 60;
+PSF_angle = 0;
+PSF_true = fspecial("motion",PSF_length,PSF_angle);
+
+img_distort_ft = fft2(img_0to1);
+PSF_fourier = psf2otf(PSF_true, [M,N]);
+img_blurred_ft = img_distort_ft .* PSF_fourier ;
+img_blurred = ifft2(img_blurred_ft);
+
+
+subplot(1,2,1); 
+imshow(img_0to1);
+
+subplot(1,2,2); 
+imshow(img_blurred);
+
 %% Add noise (Skipped for now)
 
 %% Psuedoinverse filter
-img_fouriertransformed = fft2(img_distort);
+%ändrat till att räkna med fouriertransformerade blurrade bilden
+
 PSF_fourier = psf2otf(PSF_true, [M,N]) %gör om psf till fouriervärden
-img_restored = ifft2(img_fouriertransformed ./PSF_fourier)
+img_restored = ifft2(img_blurred_ft ./PSF_fourier);
 imshow(img_restored);
+
+%%
+%pesudoinverse 
