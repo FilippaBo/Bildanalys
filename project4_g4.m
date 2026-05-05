21cefc52ac9de1bef2210cf4f1a68a3fbae0ef21
corneliaforss <corneliaforss@Corres-MacBook-Air.local>
2026-05-05 10:02:01 +0200

    har bara copypaste samma

--- project4_g4.m
+++ project4_g4.m
@@ -65,24 +65,60 @@ subplot(1,2,2);
 imshow(img_blurred_noise_ft);
 
 %chattens förslag
-img_blurred_noise_ft = fft2(img_blurred_noise);
-subplot(1,2,2);
-imshow(log(1+abs(fftshift(img_blurred_noise_ft))), []);
+%img_blurred_noise_ft = fft2(img_blurred_noise);
+%subplot(1,2,2);
+%imshow(log(1+abs(fftshift(img_blurred_noise_ft))), []);
 
-%% Psuedoinverse filter
+%% Psuedoinverse filter gammalt försök (Kan tas bort)
 
 %no noise
-img_restored = ifft2(img_blurred_ft ./PSF_fourier);
-subplot(1,2,1);
-imshow(img_restored, []);
+%img_restored = ifft2(img_blurred_ft ./PSF_fourier);
+%subplot(1,2,1);
+%imshow(img_restored, []);
 
 %ändrat till att räkna med fouriertransformerade blurrade bilden
 %with noise
-img_restored_noise = ifftshift(ifft2(fftshift(img_blurred_noise_ft ./PSF_fourier)));
-subplot(1,2,2);
-imshow(img_restored_noise, []);
+%img_restored_noise = ifftshift(ifft2(fftshift(img_blurred_noise_ft ./PSF_fourier)));
+%subplot(1,2,2);
+%imshow(img_restored_noise, []);
 
 %chattens förslag
-img_restored_noise =ifft2(img_blurred_noise_ft ./PSF_fourier);
+%img_restored_noise =ifft2(img_blurred_noise_ft ./PSF_fourier);
+%subplot(1,2,2);
+%imshow(real(img_restored_noise), []);
+
+%% Pseudoinverse nytt försök
+
+
+epsilon = 0.09; %Låga värden ger mycket noise, höga värden dålig detalj
+
+PSF_zero = zeros(size(PSF_fourier)); %Gör en lika stor matris med bara 0:or
+Cutoff_matrix = abs(PSF_fourier) >= epsilon; %Bildar en boolean matris, True där värden är större än epsilon, alltså okej att ta inverse på
+
+%imshow (Cutoff_matrix)
+
+PSF_zero(Cutoff_matrix) = 1 ./PSF_fourier(Cutoff_matrix); % När Cutoff är true ersätts nollor med inversen av H (PSF_fourier)
+
+img_restored = ifft2(img_blurred_ft .*PSF_zero); % Utan noise
+subplot(1,2,1);
+imshow(img_restored);
+
+%img_blurred_noise_ft är noiset i fourier, som multipliceras med
+%Nollmatrisen (där nollorna ersätts på alla ställen "som spelar roll")
+img_restored_noise = ifft2(img_blurred_noise_ft .* PSF_zero); %
 subplot(1,2,2);
-imshow(real(img_restored_noise), []);
+imshow(img_restored_noise);
+
+
+%% simpla sättet att köra Wiener, nästa steg är att ersätta K med S och ta avrage mellan övriga delar av bilden (se star treak exemplet från slides
+%obs ej klar
+K = 0.001;
+
+G = fft2(img_blurred_noise);  
+H = PSF_fourier;
+
+F_hat = (conj(H) ./ (abs(H).^2 + K)) .* G; %se slide 57 för formel
+
+img_wiener = ifft2(F_hat);
+
+imshow(img_wiener, []);
