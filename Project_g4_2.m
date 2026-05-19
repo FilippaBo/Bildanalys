99ee3203078085107022ebdbc12aa65c1dd7f8f6
Osclindg <osse.lingling@gmail.com>
2026-05-19 16:25:51 +0200

    Updatering
    Inte helt klart än

--- Project_g4_2.m
+++ Project_g4_2.m
@@ -1,37 +1,33 @@
 img_raw = 'Xray.png';
 
-%% Gör om bild till 0-1 värden
-img_pixels = imread(img_raw);
-img_0to1 = im2double(img_pixels);
+%% Creates the images
+img_pixels = imread(img_raw); %Reads the raw picture
+img_0to1 = im2double(img_pixels); %Rescales pixel values to stay between 0 and 1
 
-[M, N] = size(img_0to1);
+[M, N] = size(img_0to1); %Reads the size of the image
+max(max(img_0to1)); %Checks the value of the highest pixel in the image
 
-%%
-%kollar maxvärdet på högsta pixeln
-max(max(img_0to1))
+%% First attempt at blurring, without using fourier transform
+%PSF_length = 60;
+%PSF_angle = 0;
+%PSF_true = fspecial("motion",PSF_length,PSF_angle);
+%
+%img_distort = imfilter(img_0to1, PSF_true, "circular", "conv"); 
+%
+%subplot(1,2,1); 
+%imshow(img_0to1);
+%
+%subplot(1,2,2); 
+%imshow(img_distort);
 
-%% 
+%% Blurring using fourier transform 
 PSF_length = 60;
 PSF_angle = 0;
-PSF_true = fspecial("motion",PSF_length,PSF_angle);
+PSF_true = fspecial("motion",PSF_length,PSF_angle); %Creates a PSF, with a top hat function
 
-img_distort = imfilter(img_0to1, PSF_true, "circular", "conv");
-
-subplot(1,2,1); 
-imshow(img_0to1);
-
-subplot(1,2,2); 
-imshow(img_distort);
-
-%%
-% nytt sätt att blurra fast med fourier 
-PSF_length = 60;
-PSF_angle = 0;
-PSF_true = fspecial("motion",PSF_length,PSF_angle);
-
-img_distort_ft = fft2(img_0to1); %gör om bilden till fourierdomänen så f-> F
-PSF_fourier = psf2otf(PSF_true, [M,N]); %gör om psf till fouriervärden h -> H
-img_blurred_ft = img_distort_ft .* PSF_fourier ; %F * H = G
+img_0to1_ft = fft2(img_0to1); % f -> F, turns original image into fourier
+PSF_fourier = psf2otf(PSF_true, [M,N]); % h -> H, turns psf image into fourier
+img_blurred_ft = img_0to1_ft .* PSF_fourier ; % This is when the blurred is created (G = F * H)
 img_blurred = ifft2(img_blurred_ft); % G -> g
 
 
@@ -40,7 +36,7 @@ imshow(img_0to1); %originalbild f
 title('f', 'FontSize', 24)
 
 subplot(2,3,4);
-imshow(log(abs(fftshift(img_distort_ft))),[]); %originalbild i ft, F
+imshow(log(abs(fftshift(img_0to1_ft))),[]); %originalbild i ft, F
 title('F', 'FontSize', 24)
 
 subplot(2,3,2);
@@ -60,47 +56,44 @@ imshow(log(abs(fftshift(img_blurred_ft))),[]); %blurrad bild i ft, G
 title('G', 'FontSize', 24)
 
 %% Add noise 
-img_blurred_noise = imnoise(img_blurred, "gaussian");
+img_blurred_noise = imnoise(img_blurred, "gaussian"); % This adds noise 
 subplot(1,2,1);
 imshow(img_blurred_noise);
 title('Blurred image with noise', 'FontSize', 24);
 
 %vår kod
-img_blurred_noise_ft = fftshift(fft2(img_blurred_noise));
+img_blurred_noise_ft = fft2(img_blurred_noise); % Same thing in fourier
 subplot(1,2,2);
 imshow(img_blurred_noise_ft);
 title('Blurred image with noise in fourier domain', 'FontSize', 24)
 
-%chattens förslag
-%img_blurred_noise_ft = fft2(img_blurred_noise);
-%subplot(1,2,2);
-%imshow(log(1+abs(fftshift(img_blurred_noise_ft))), []);
-%img_blurred_noise_ft = fft2(img_blurred_noise);
-%subplot(1,2,2);
-%imshow(log(1+abs(fftshift(img_blurred_noise_ft))), []);
-
-%% Pseudoinverse nytt försök
+%% Pseudoinverse filter
 
 
-epsilon = 0.1; %Låga värden ger mycket noise, höga värden dålig detalj
+epsilon = 0.1; % Example value for epsilon
 
-PSF_zero = zeros(size(PSF_fourier)); %Gör en lika stor matris med bara 0:or
-Cutoff_matrix = abs(PSF_fourier) >= epsilon; %Bildar en boolean matris, True där värden är större än epsilon, alltså okej att ta inverse på
+PSF_epsilon = zeros(size(PSF_fourier)); % Creates a Matrix the size of the PSF, filled with zeros 
+Boolean_matrix = abs(PSF_fourier) >= epsilon; % Creates a boolean matrix, assigning all values bigger then epsilon with true
 
-%imshow (Cutoff_matrix)
+% The zeros in the zero matrix are replace in all positions where the
+% matrix is "True". Those zeros are replaced with the value 1/H.
+% PSF_espilon is now the same thing as PSF_fourier, but with every value
+% less than epsilon being 0
+PSF_epsilon(Boolean_matrix) = 1 ./PSF_fourier(Boolean_matrix);
 
-PSF_zero(Cutoff_matrix) = 1 ./PSF_fourier(Cutoff_matrix); % När Cutoff är true ersätts nollor med inversen av H (PSF_fourier)
-
-img_restored = ifft2(img_blurred_ft .*PSF_zero); % Utan noise
+% This does two things: First (G * (1/H)) is calculated, secondly, the
+% inverse fourier transform is applied to go back into the picture domain
+img_restored = ifft2(img_blurred_ft .*PSF_epsilon); 
 subplot(1,2,1);
-imshow(img_restored);
+imshow(img_restored, []);
+title('Restored image no noise', 'FontSize', 24)
 
-%img_blurred_noise_ft är noiset i fourier, som multipliceras med
-%Nollmatrisen (där nollorna ersätts på alla ställen "som spelar roll")
-img_restored_noise = ifft2(img_blurred_noise_ft .* PSF_zero);
+%This does the same thing the comment above describes, but this time G also
+%contains noise
+img_restored_noise = ifft2(img_blurred_noise_ft .* PSF_epsilon);
 subplot(1,2,2);
-imshow(real(img_restored_noise), []);
-imshow(img_restored_noise);
+imshow(img_restored_noise, []);
+title('Restored image with noise', 'FontSize', 24)
 
 %% simpla sättet att köra Wiener, nästa steg är att ersätta K med S och ta avrage mellan övriga delar av bilden (se star treak exemplet från slides
 %obs ej klar
